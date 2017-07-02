bb6467deb39a798b1084cf688303735b8e2fe914
Joselito Júnior <eu@joselitojunior.com>
2017-07-02 20:43:15 -0300

    deployer atualizado

--- d.bat
+++ d.bat
@@ -1,5 +1,6 @@
 @echo off
-@echo [= JOSELITO.SPACE DEPLOYER =]
+@color 9f
+@echo [= JOSELITO.SPACE DEPLOYER 3000 =]
 @call git add .
 @call git commit -am "%~1"
 @call git push origin master
@@ -11,4 +12,4 @@
 @call git commit -am "Update: %~1 %date% %time%"
 @call git push origin gh-pages
 @cd ..
-@echo DONE
+@echo [= DONE =]
