290c351f0fd8b106e9e005dbc82f78f41155a173
Joselito Júnior <eu@joselitojunior.com>
2017-07-02 19:53:21 -0300

    app id

--- deployer.bat
+++ deployer.bat
@@ -2,3 +2,13 @@
 @echo [= JOSELITO.SPACE DEPLOYER =]
 @call git add .
 @call git commit -am "%~1"
+@call git push origin master
+@call hexo clean
+@call hexo generate
+@call xcopy "public" "gh-pages" /i/e/r/y
+@cd gh-pages
+@call git add .
+@call git commit -am "Update: %~1 %date% %time%"
+@call git push origin gh-pages
+@cd ..
+@echo DONE
